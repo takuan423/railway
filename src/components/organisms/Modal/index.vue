4bdf4eefdcf0c3cd4518fe815def7a3b0bc4e035
菊池卓也 <takuya.kikuchi@i-studio.co.jp>
2019-06-06 19:05:36 +0900

    機能実装

--- src/components/organisms/Modal/index.vue
+++ src/components/organisms/Modal/index.vue
@@ -1,6 +1,10 @@
 <template lang="pug">
-a.o-modal(v-on:click="openModal" :positionX="`${positionY}`" :positionY="`${positionY}`")
-  |{{name}}
+.o-modal
+  a.o-modal__button(v-on:click="openModal" :positionX="`${positionY}`" :positionY="`${positionY}`")
+    |{{name}}
+  .o-modal__content(:style="`display:${switchModal}`")
+    .o-modal__content__inner
+      .o-modal__content__background(v-on:click="closeModal")
 </template>
 
 <script>
@@ -9,13 +13,21 @@ export default {
   },
   props: {
     positionX: {
-      type: String
+      type: String,
+      required: true
     },
     positionY: {
-      type: String
+      type: String,
+      required: true
     },
     name: {
-      type: String
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      switchModal: 'none'
     }
   },
   methods: {
@@ -23,13 +35,38 @@ export default {
       const posX = e.target.getAttribute('positionx')
       const posY = e.target.getAttribute('positiony')
       console.log(posX, posY)
+      this.switchModal = 'block'
+    },
+    closeModal(e) {
+      console.log(e)
+      this.switchModal = 'none'
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.o-modal
+.o-modal__button
   display block
   padding 5px
+
+.o-modal__content
+  display none
+  position fixed
+  top 0
+  left 0
+  right 0
+  bottom 0
+  margin auto
+  z-index 999
+
+.o-modal__content__inner
+  width 100%
+  height 100%
+
+.o-modal__content__background
+  width 100%
+  height 100%
+  opacity 0.7
+  background #000
 </style>
